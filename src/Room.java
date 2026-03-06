f98089ecac27195aa3ca2b3b16c517c14f85affb
Yaswanth-Reddy-Papari <papariyaswanthreddy@gmail.com>
2026-03-06 15:29:20 +0530

    UC2 Room Types and Static Availability

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,18 @@
-public class Room {
+public abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int beds, int size, double price) {
+        this.numberOfBeds = beds;
+        this.squareFeet = size;
+        this.pricePerNight = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
 }
